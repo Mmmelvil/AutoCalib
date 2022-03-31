eda71881fc27b39e2c360e4dd98721edab879f51
jinch <jinchenzeng@outlook.com>
2022-04-01 00:59:36 +0200

    plot object

--- Objects.py
+++ Objects.py
@@ -1,10 +1,14 @@
-
+import json
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 ## order down-left, top-left, down-right, top-right
-door1_inside_frame = [[[184.5, 400, 0],
+door1_inside_frame = [[184.5, 400, 0],
                       [184.5, 400, 234.5],
                       [306.5, 400, 0],
-                      [306.5, 400, 234.5]]]
+                      [306.5, 400, 234.5]]
 
 door1_outside_frame = [[180.5, 400, 0],
                       [180.5, 400, 238],
@@ -51,8 +55,8 @@ door2_handle = [[320.5, 110, 125]]
 # switch of door 2
 door2_switch = [[320.5, 93, 141],
                 [320.5, 93, 166],
-                [320.5, 103, 141],
-                [320.5, 103, 166]]
+                [320.5, 86.5, 141],
+                [320.5, 86.5, 166]]
 
 
 # fire blanket box
@@ -93,3 +97,35 @@ wall_screen = [[-200, 244, 155.5],
 # lamp
 lamp = [[-200, 233, 213]]
 
+fig = plt.figure(figsize=(4,4))
+ax = fig.add_subplot(111, projection='3d')
+# ax.set_aspect('equal')
+def plot_object(lst):
+    arr = np.array(lst)
+    ax.scatter(arr[:, 0], arr[:, 1], arr[:, 2])
+    return None
+
+plot_object(working_bench)
+plot_object(door2_inside_frame)
+plot_object(door2_outside_frame)
+plot_object(door2_depth )
+plot_object(door2_handle )
+plot_object(door2_switch )
+plot_object(fire_blanket_box)
+plot_object(door1_inside_frame)
+plot_object(door1_outside_frame)
+plot_object(door1_handle)
+
+plot_object(window1_frame)
+plot_object(window2_frame)
+plot_object(two_window_outise_frame)
+
+plot_object(patslide)
+plot_object(lamp)
+plot_object(wall_screen)
+# ax.scatter(getXYZ(door2_switch))
+# ax.scatter(getXYZ(fire_blanket_box))
+plt.show()
+
+
+## 少了window depth 还有button的relative distance
